1017c0336fe4338b5a4e0a1ef47aa7e1f0071784
Sujata Saini <sujata.saini.ai@gmail.com>
2022-05-01 15:55:15 +0900

    changes

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -29,7 +29,7 @@ if($_POST) {
 
    // Set Message
    $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
+   $message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
